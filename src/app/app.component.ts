0db453680a677a8ad6f877456904ae772196c2ff
hsiaofongw <hsiaofong.w@gmail.com>
2022-03-02 15:22:54 +0800

    feat: 增加 hover 效果

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,19 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { solveRecursive, solveSudoku } from './solve';
+
+type CellDatum = {
+  content: string;
+  editable: boolean;
+  type: 'condition' | 'answer';
+};
+
+type Option = {
+  label: string;
+  value: string;
+};
+
+type Mode = 'readOnlyMode' | 'conditionWriteOnlyMode' | 'answerWriteOnlyMode' | 'writeMode';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +23,23 @@ import { solveRecursive, solveSudoku } from './solve';
 export class AppComponent {
   title = 'sudoku-solver';
 
+  gridData: CellDatum[][] = [];
+
+  modeOptions: Option[] = [
+    { label: '查看', value: 'readOnlyMode' },
+    { label: '仅条件可编辑', value: 'conditionWriteOnlyMode' },
+    { label: '仅答案可编辑', value: 'answerWriteOnlyMode' },
+    { label: '条件和答案都可编辑', value: 'writeMode' },
+  ];
+
+  readonly defaultMode: Mode = 'readOnlyMode';
+
+  _modeForm = new FormGroup({
+    mode: new FormControl('readOnlyMode'),
+  });
+
   ngOnInit(): void {
+
     const input = [
       ['5', '3', '.', '.', '7', '.', '.', '.', '.'],
       ['6', '.', '.', '1', '9', '5', '.', '.', '.'],
@@ -21,7 +51,29 @@ export class AppComponent {
       ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
       ['.', '.', '.', '.', '8', '.', '.', '7', '9'],
     ];
-    solveSudoku(input);
-    console.log(input);
+
+    this.gridData = input.map((row) =>
+      row.map((char) => ({ content: char, editable: false, type: char === '.' ? 'answer' : 'condition' }))
+    );
+
+    this._modeForm.valueChanges.subscribe((formValue) => {
+      this._setMode(formValue.mode ?? this.defaultMode);
+    });
+  }
+
+  private _setMode(mode: Mode): void {
+    if (mode === 'readOnlyMode') {
+      this.gridData = this.gridData.map(row => row.map(cell => ({ ...cell, editable: false })));
+    }
+    else if (mode === 'answerWriteOnlyMode') {
+      this.gridData = this.gridData.map(row => row.map(cell => ({ ...cell, editable: cell.type === 'answer' })));
+    }
+    else if (mode === 'conditionWriteOnlyMode') {
+      this.gridData = this.gridData.map(row => row.map(cell => ({ ...cell, editable: cell.type === 'condition' })));
+    }
+    else if (mode === 'writeMode') {
+      this.gridData = this.gridData.map(row => row.map(cell => ({ ...cell, editable: true })));
+    }
+    else { }
   }
 }
