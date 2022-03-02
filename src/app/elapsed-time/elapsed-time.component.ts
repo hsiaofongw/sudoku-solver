0d00a5d4456e24e04795efba4b43466997eec139
hsiaofongw <hsiaofong.w@gmail.com>
2022-03-02 18:55:49 +0800

    fix: 修复了计时显示错误问题

--- src/app/elapsed-time/elapsed-time.component.ts
+++ src/app/elapsed-time/elapsed-time.component.ts
@@ -9,6 +9,8 @@ export class ElapsedTimeComponent implements OnInit {
   @Input()
   elapsedMs: number = 0;
 
+  years: number = 0;
+  days: number = 0;
   hours: number = 0;
   minutes: number = 0;
   seconds: number = 0;
@@ -19,9 +21,11 @@ export class ElapsedTimeComponent implements OnInit {
   }
 
   ngOnChanges(): void {
-    this.hours = this.elapsedMs / (60 * 60 * 1000);
-    this.minutes = this.elapsedMs / (60 * 1000);
-    this.seconds = this.elapsedMs / 1000;
+    this.years = (Math.floor(this.elapsedMs / (365 * 24 * 60 * 60 * 1000)));
+    this.days = (Math.floor(this.elapsedMs / (24 * 60 * 60 * 1000))) % 365;
+    this.hours = (Math.floor(this.elapsedMs / (60 * 60 * 1000))) % 24;
+    this.minutes = (Math.floor(this.elapsedMs / (60 * 1000))) % 60;
+    this.seconds = (Math.floor(this.elapsedMs / 1000)) % 60;
   }
 
 }
