cbd6fdb3c692d1b8a41984dea825b52368af06b6
hsiaofongw <hsiaofong.w@gmail.com>
2022-03-02 14:02:05 +0800

    pass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { solveRecursive, solveSudoku } from './solve';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'sudoku-solver';
+
+  ngOnInit(): void {
+    const input = [
+      ['5', '3', '.', '.', '7', '.', '.', '.', '.'],
+      ['6', '.', '.', '1', '9', '5', '.', '.', '.'],
+      ['.', '9', '8', '.', '.', '.', '.', '6', '.'],
+      ['8', '.', '.', '.', '6', '.', '.', '.', '3'],
+      ['4', '.', '.', '8', '.', '3', '.', '.', '1'],
+      ['7', '.', '.', '.', '2', '.', '.', '.', '6'],
+      ['.', '6', '.', '.', '.', '.', '2', '8', '.'],
+      ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
+      ['.', '.', '.', '.', '8', '.', '.', '7', '9'],
+    ];
+    solveSudoku(input);
+    console.log(input);
+  }
 }
--- src/app/solve.ts
+++ src/app/solve.ts
@@ -1,89 +1,96 @@
-/**
- Do not return anything, modify board in-place instead.
- */
- function solveSudoku(board: string[][]): void {
-  solveRecursive(board, 0);
-};
+export function solveSudoku(board: string[][]): void {
+  solveRecursive(board, 0, { done: false });
+}
 
 function getCoordFromIdx(idx: number): number[] {
-  const rowIdx = Math.floor(idx / 9)
-  const colIdx = Math.floor(idx % 9);
+  const rowIdx = Math.floor(idx / 9);
+  const colIdx = idx % 9;
   return [rowIdx, colIdx];
 }
 
-
-function getOptions(board: string[][], idx: number): string[] {
-  const optionMap: Record<string, boolean> = {};
-  for (let i = 0; i < 0; i++) {
-      optionMap[i.toString()] = true;
-  }
-  
-  const coord = getCoordFromIdx(idx);
-  const rowIdx = coord[0];
-  const colIdx = coord[1];
-  for (let i = 0; i < 9; i++) {
-      if (board[i][colIdx] !== '.') {
-          optionMap[board[i][colIdx]] = false;
-      }
-  }
-  
-  for (let j = 0; j < 9; j++) {
-      if (board[rowIdx][j] !== '.') {
-          optionMap[board[rowIdx][j]] = false;
-      }
-  }
-  
+function getSubboxIndices(rowIdx: number, colIdx: number): number[][] {
   const subBoxColIdx = Math.floor(colIdx / 3);
   const subBoxRowIdx = Math.floor(rowIdx / 3);
   const subBoxMinColIdx = subBoxColIdx * 3;
   const subBoxMaxColIdx = (subBoxColIdx + 1) * 3 - 1;
   const subBoxMinRowIdx = subBoxRowIdx * 3;
   const subBoxMaxRowIdx = (subBoxRowIdx + 1) * 3 - 1;
+  const coords: number[][] = [];
   for (let i = subBoxMinRowIdx; i <= subBoxMaxRowIdx; i++) {
-      for (let j = subBoxMinColIdx; j <= subBoxMaxColIdx; j++) {
-          if (board[i][j] !== '.') {
-              optionMap[board[i][j]] = false;
-          }
-      }
+    for (let j = subBoxMinColIdx; j <= subBoxMaxColIdx; j++) {
+      coords.push([i, j]);
+    }
   }
-  
-  const options: string[] = [];
-  for (const key in optionMap) {
-      if (optionMap[key]) {
-          options.push(key);
-      }
-  }
-  
-  return options;
+
+  return coords;
 }
 
-function solveRecursive(board: string[][], idx: number): boolean {
-  if (idx === 81) {
-      return true;
-  }
-  
+function getOptions(board: string[][], idx: number): string[] {
   const coord = getCoordFromIdx(idx);
   const rowIdx = coord[0];
   const colIdx = coord[1];
-  const val = board[rowIdx][colIdx];
-  if (val !== '.') {
-      return solveRecursive(board, idx+1);
+
+  const optionMap: Record<string, boolean> = {};
+  for (let i = 1; i <= 9; i++) {
+    optionMap[i.toString()] = true;
   }
+
+  for (let i = 0; i < 9; i++) {
+    if (board[i][colIdx] !== '.') {
+      optionMap[board[i][colIdx]] = false;
+    }
+  }
+
+  for (let j = 0; j < 9; j++) {
+    if (board[rowIdx][j] !== '.') {
+      optionMap[board[rowIdx][j]] = false;
+    }
+  }
+
+  const subbox = getSubboxIndices(rowIdx, colIdx);
+  for (const coord of subbox) {
+    const boardVal = board[coord[0]][coord[1]];
+    if (boardVal !== '.') {
+      optionMap[boardVal] = false;
+    }
+  }
+
+  const options: string[] = [];
+  for (const key in optionMap) {
+    if (optionMap[key]) {
+      options.push(key);
+    }
+  }
+
+  return options;
+}
+
+export function solveRecursive(
+  board: string[][],
+  idx: number,
+  state: { done: boolean }
+): void {
+  const coord = getCoordFromIdx(idx);
+  const rowIdx = coord[0];
+  const colIdx = coord[1];
+
+  if (idx === board.length * board[0].length) {
+    state.done = true;
+    return;
+  }
+
+  const val = board[rowIdx][colIdx];
+  if (val !== '.') solveRecursive(board, idx + 1, state);
   else {
-      const options = getOptions(board, idx);
-      if (options.length === 0) {
-          board[rowIdx][colIdx] = '.';
-          return false;
+    const options = getOptions(board, idx);
+    for (const option of options) {
+      board[rowIdx][colIdx] = option;
+      solveRecursive(board, idx + 1, state);
+      if (state.done) {
+        return;
+      } else {
+        board[rowIdx][colIdx] = '.';
       }
-      for (const option of options) {
-          board[rowIdx][colIdx] = option;
-          if (solveRecursive(board, idx+1)) {
-              return true;
-          }
-          else {
-              board[rowIdx][colIdx] = '.';
-          }
-      }
-      return false;
+    }
   }
 }
